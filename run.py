4cd0d730ae6b4948df36513f9bb203a3b6c1c575
Airdrie86 <gibby86@hotmail.com>
2021-10-15 15:20:15 +0000

    Change initial coding

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 import random
-from words import words
+
+words = ["BEEP", "DART", "CORE", "FROG", "HEAT", "MOLD", "WORK",
+ "AVAIL", "CAMEL", "EARTH", "GIANT", "KARMA", "MOULD", "NINJA", 
+ "ANTHEM", "BREATH", "CRAYON", "ZODIAC", "TRAVEL", 
+ "AIRPORT", "BOILING", "CLUSTER", "UPTIGHT", "TOURISM"]
+
+WORD = random.choice(words)
+print(WORD)
 
 
-def get_the_words():
-    word = random.choice(words)
-return word.upper()
-
-def guess_the_word(game):
-    
