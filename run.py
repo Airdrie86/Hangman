c9009ec50d9f1b4b14d70a91a7499844564c314b
Airdrie86 <gibby86@hotmail.com>
2021-10-15 17:44:41 +0000

    Add game play function

--- run.py
+++ run.py
@@ -1,11 +1,22 @@
 import random
-
-words = ["BEEP", "DART", "CORE", "FROG", "HEAT", "MOLD", "WORK",
- "AVAIL", "CAMEL", "EARTH", "GIANT", "KARMA", "MOULD", "NINJA", 
- "ANTHEM", "BREATH", "CRAYON", "ZODIAC", "TRAVEL", 
- "AIRPORT", "BOILING", "CLUSTER", "UPTIGHT", "TOURISM"]
-
-WORD = random.choice(words)
-print(WORD)
+import sys
+import operator
+import os
+from words import words_available
 
 
+def random_word():
+    """
+    This is where we choose a random word from our words list
+    """
+    word = random.choice(words_available)
+    return word.upper()
+
+
+def game(word):
+    """
+    This will hold the main game fuctions
+    """
+    max_tries = 6
+    guess = "_" * len(word)
+  
--- words.py
+++ words.py
@@ -1,4 +1,4 @@
-words = ["BEEP", "DART", "CORE", "FROG", "HEAT", "MOLD", "WORK",
+words_available = ["BEEP", "DART", "CORE", "FROG", "HEAT", "MOLD", "WORK",
  "AVAIL", "CAMEL", "EARTH", "GIANT", "KARMA", "MOULD", "NINJA", 
  "ANTHEM", "BREATH", "CRAYON", "ZODIAC", "TRAVEL", 
  "AIRPORT", "BOILING", "CLUSTER", "UPTIGHT", "TOURISM"]
