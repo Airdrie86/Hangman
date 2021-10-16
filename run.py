f6f65012a47b9675389764f32e0e1ebeac688f30
Airdrie86 <gibby86@hotmail.com>
2021-10-16 22:14:12 +0000

    Adjust hangman welcome screen

--- run.py
+++ run.py
@@ -86,7 +86,7 @@ used_letters = []
 guessed_words = []
 # Introducing the game to the user
 print("Greetings Traveller! The game of Hangman awaits you!")
-print(hangman[0])
+print(hangman[6])
 name = input("Please enter your name:\n")
 print(f"Hello {name}! Welcome to Hangman!")
 # Randomly choosing a word for the game
